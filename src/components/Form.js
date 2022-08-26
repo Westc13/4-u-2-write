a72134569b5a68c1fae2c1bd7267795e84404eba
s-avery <s_avery@riseup.net>
2022-08-26 17:15:54 -0400

    instructions and some css cleanup

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,39 +1,36 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUndo } from "@fortawesome/free-solid-svg-icons";
-import { useContext, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import TimerContext from "../contexts/TimerContext";
 import TimerContext from "../contexts/TimerContext";
 
 const Form = () => {
-  const { time } = useContext(TimerContext);
-  const { time } = useContext(TimerContext);
-  const [userText, setUserText] = useState("");
-  const [isIdle, setIsIdle] = useState(true);
-  const idleInterval = 15000;
+	const { time } = useContext(TimerContext);
+	const [userText, setUserText] = useState("");
+	const [isIdle, setIsIdle] = useState(true);
+	const idleInterval = 15000;
 
 	const handleChange = (e) => {
 		setUserText(e.target.value);
-    setIsIdle(false);
+		setIsIdle(false);
 	};
 
-  const handleKeyUp = () => {
-    setIsIdle(true);
-  };
+	const handleKeyUp = () => {
+		setIsIdle(true);
+	};
 
-  useEffect(() => {
-    let countDown = time;
-    let countDown = time;
-    const myInterval = setInterval(() => {
-      if (countDown > 0) {
-        if (isIdle) toast("15 seconds");
-        else clearInterval(myInterval);
-        countDown -= 15;
-      }
-      if (countDown === 0) {
-        clearInterval(myInterval);
-      }
-    }, idleInterval);
+	useEffect(() => {
+		let countDown = time;
+		const myInterval = setInterval(() => {
+			if (countDown > 0) {
+				if (isIdle) toast("15 seconds");
+				else clearInterval(myInterval);
+				countDown -= 15;
+			}
+			if (countDown === 0) {
+				clearInterval(myInterval);
+			}
+		}, idleInterval);
 
 		return () => {
 			clearInterval(myInterval);
@@ -49,50 +46,30 @@ const Form = () => {
 	//on keyUp start checker (setTimeout)
 	//on keyDown stop checker clearInterval reset lapse time
 
-  return (
-    <form className="Main__form">
-      <fieldset>
-        <label htmlFor="journal" className="sr-only">
-          Write here
-        </label>
-        <textarea
-          name="journal"
-          id="journaljournal"
-          cols="30"
-          rows="30"
-          value={userText}
-          onChange={handleChange}
-          onKeyUp={handleKeyUp}
-          onMouseUp={() => {
-            setIsIdle(true);
-          }}
-          onMouseDown={() => {
-            setIsIdle(false);
-          }}
-          onMouseUp={() => {
-            setIsIdle(true);
-          }}
-          onMouseDown={() => {
-            setIsIdle(false);
-          }}
-        ></textarea>
-      </fieldset>
-      <fieldset className="Main__promptfield">
-        <button className="Main__addBtn button">Add</button>
-
-        <div className="Main__addPrompt">
-          <label htmlFor="prompt" className="sr-only">
-            Enter your prompt
-          </label>
-          <input id="prompt" type="text" className="Main__inputPrompt" />
-          <span className="Main__undoBtn">
-            <FontAwesomeIcon icon={faUndo} className="icon__Undo" />
-          </span>
-          <button className="Main__confirmBtn button">Confirm</button>
-        </div>
-      </fieldset>
-    </form>
-  );
+	return (
+		<form className="Main__form">
+			<fieldset>
+				<label htmlFor="journal" className="sr-only">
+					Write here
+				</label>
+				<textarea
+					name="journal"
+					id="journal"
+					cols="30"
+					rows="30"
+					value={userText}
+					onChange={handleChange}
+					onKeyUp={handleKeyUp}
+					onMouseUp={() => {
+						setIsIdle(true);
+					}}
+					onMouseDown={() => {
+						setIsIdle(false);
+					}}
+				></textarea>
+			</fieldset>
+		</form>
+	);
 };
 
 export default Form;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,38 @@
 // !IMPORT ZONE
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
-import { useContext, useContext, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
 import TimerContext from "../contexts/TimerContext";
-import TimerContext from "../contexts/TimerContext";
+import toast from "react-hot-toast";
+import { FaSun } from "react-icons/fa";
 
 const Home = ({
-  prompts,
-  setPrompts,
-  POTD,
-  setPOTD,
-  currentDay,
-  setCurrentDay,
-  timeCheck,
+	prompts,
+	setPrompts,
+	POTD,
+	setPOTD,
+	currentDay,
+	setCurrentDay,
+	timeCheck,
 }) => {
+	// !STATE ZONE
 	const { setTime } = useContext(TimerContext);
-
 	const [userSelection, setUserSelection] = useState("placeholder");
+	// *Navigate
+	const navigate = useNavigate();
 
+	// !FUNCTION ZONE
 	const handleOnChange = (e) => {
 		setUserSelection(e.target.value);
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
+		if (userSelection === "placeholder") {
+			toast.error("You must choose a writing time.");
+		} else {
+			navigate("/main");
+		}
 		timeCheck();
 	};
 
@@ -41,7 +49,7 @@ const Home = ({
 					</div>
 
 					<button className="myButton">
-						Toggle Light/Dark Theme
+						<FaSun />
 					</button>
 				</header>
 
@@ -61,6 +69,7 @@ const Home = ({
 							<option value="placeholder" disabled>
 								Choose your writing time
 							</option>
+							{/* //TODO update this to 15 mins */}
 							<option value={130}>15 minutes</option>
 							<option value={1800}>30 minutes</option>
 							<option value={2700}>45 minutes</option>
@@ -70,28 +79,23 @@ const Home = ({
 						<label htmlFor="writingTime" className="sr-only">
 							Choose your writing time
 						</label>
-						<Link to="/main">
-							<button className="home__goButton darkBtn">
-								Go
-							</button>
-						</Link>
+
+						<button className="home__goButton lightBtn">Go</button>
 					</form>
 				</main>
 
-        <aside className="home__aside">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-            labore, neque distinctio voluptatem vero perferendis suscipit, ullam
-            at ipsa earum cum cupiditate perspiciatis laborum adipisci porro
-            exercitationem eveniet non iste!
-          </p>
-        </aside>
-        <footer className="home__footer">
-          created at juno with unending spite
-        </footer>
-      </div>
-    </div>
-  );
+				<aside className="home__aside">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Assumenda, labore, neque distinctio voluptatem vero
+						perferendis suscipit, ullam at ipsa earum cum cupiditate
+						perspiciatis laborum adipisci porro exercitationem
+						eveniet non iste!
+					</p>
+				</aside>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
