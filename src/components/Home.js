0ebdd3cb5054c4ab8422f1595e219d2e3c922fbe
KevinBruce <kevinlawbruce@gmail.com>
2022-08-31 15:52:17 -0400

    media queries

--- src/components/Home.js
+++ src/components/Home.js
@@ -68,7 +68,7 @@ const Home = ({
               <option value="placeholder" disabled>
                 Choose your writing time
               </option>
-              <option value={35}>15 minutes</option>
+              <option value={900}>15 minutes</option>
               <option value={1800}>30 minutes</option>
               <option value={2700}>45 minutes</option>
               <option value={3600}>60 minutes</option>
